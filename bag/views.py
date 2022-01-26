ca9cbec12ab23df610c5738286cf4b0bd7230386
lilblupig <lilblupig@gmail.com>
2022-01-26 22:14:53 +0000

    Bag app setup

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def view_bag(request):
+    """ View to show the contents of the shopping bag """
+    return render(request, 'bag/bag.html')
