9325b915df7bc1ddf3f3c4788261e99ef92897ef
lilblupig <lilblupig@gmail.com>
2022-02-19 18:45:52 +0000

    Change checkout view code order per source mats

--- checkout/views.py
+++ checkout/views.py
@@ -2,20 +2,17 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
-from bag.contexts import bag_contents
+from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
-from .forms import OrderForm
+from bag.contexts import bag_contents
 
 import stripe
 
 
-# Create your views here.
-
-
 def checkout(request):
     """ Check bag has content and render order template """
-
+    
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -74,20 +71,20 @@ def checkout(request):
             messages.error(request, "There's nothing in your bag at the moment")
             return redirect(reverse('products'))
 
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
             Did you forget to set it in your environment?')
-
-    order_form = OrderForm()
 
     template = 'checkout/checkout.html'
     context = {
